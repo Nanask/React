a375223edfdd99b8111d6802ed987d971b3e6c76
Nanask <ybhag1005@daum.net>
2021-09-09 16:50:43 +0900

    firebase

--- react-firebase-bbs01/src/comps/BBsMain.jsx
+++ react-firebase-bbs01/src/comps/BBsMain.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../css/bbs.css";
+import { firestore } from "../config/FirebaseConfig";
 
 function BBsMain() {
+  let [bbsBody, setBBsBody] = useState([]);
+  const firebaseFetch = () => {
+    firestore
+      .collection("bbs")
+      .get()
+      .then((bbsList) => {
+        bbsList.forEach((bbs) => {
+          const item = bbs.data();
+          setBBsBody([
+            ...bbsBody,
+            <tr>
+              <td>{item.b_date}</td>
+              <td>{item.b_time}</td>
+              <td>{item.b_write}</td>
+              <td>{item.b_subject}</td>
+            </tr>,
+          ]);
+        });
+      });
+  };
+
+  //   bbsBody = await bbsList.map((bbs) => {
+  //     // console.log(bbs.data());
+  //     const item = bbs.data();
+  //     return (
+  //       <tr>
+  //         <td>{item.b_date}</td>
+  //         <td>{item.b_time}</td>
+  //         <td>{item.b_writer}</td>
+  //         <td>{item.b_subject}</td>
+  //       </tr>
+  //     );
+  //   });
+  // };
+
+  // 최초로 실행될 때 firebase를 가지고와서 보여주는 코드
+  useEffect(firebaseFetch, []);
+
   return (
     <table className="bbs_list">
       {/* <thead> 리엑트를 쓸때는 그냥 써주자 안써주면 경고뜬대요 */}
@@ -13,6 +52,7 @@ function BBsMain() {
           <th>제목</th>
         </tr>
       </thead>
+      <tbody>{bbsBody}</tbody>
     </table>
   );
 }
