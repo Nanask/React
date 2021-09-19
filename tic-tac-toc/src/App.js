ffcf2aeb31957dc486a60dcb3d26b74b93dff9de
Nanask <ybhag1005@daum.net>
2021-09-19 23:51:26 +0900

    틱택토시작

---
+++ tic-tac-toc/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+		<div className="box_row">
+			<button></button>
+			<button></button>
+			<button></button>
+		</div>
+		<div className="box_row">
+			<button></button>
+			<button></button>
+			<button></button>
+		</div>
+		<div className="box_row">
+			<button></button>
+			<button></button>
+			<button></button>
+		</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
