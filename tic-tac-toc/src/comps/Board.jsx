67f2371381a3357f61c85bfc54816d9a54aeb0a6
Nanask <ybhag1005@daum.net>
2021-09-22 23:46:00 +0900

    저장확인

--- tic-tac-toc/src/comps/Board.jsx
+++ tic-tac-toc/src/comps/Board.jsx
@@ -6,10 +6,11 @@ import {RenderSquare} from "../modules/main.jsx"
 function Board() {
 
 	const [square, setsqure] = useState(Array(9).fill("A"));
-
+// 혼자하기
 	return (
 		<div>
 			<RenderSquare square={square}/> 
+
 		</div>
 	)
 
