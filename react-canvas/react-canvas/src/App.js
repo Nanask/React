19a7d2db7f8d038551949124a2ad9708171ee897
Nanask <ybhag1005@daum.net>
2022-06-14 17:43:43 +0900

    canvas를 이용해서 사각형을 그려 좌표값 구하기

---
+++ react-canvas/react-canvas/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { DragCanvas } from "./DragCanvas";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <DragCanvas />
+      </header>
+    </div>
+  );
+}
+
+export default App;
